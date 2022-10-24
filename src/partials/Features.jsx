689b3f9eb230de051578e5276d6929851d01b46b
unknown <giri@10000>
2022-10-24 11:10:46 +0300

    added

--- src/partials/Features.jsx
+++ src/partials/Features.jsx
@@ -30,15 +30,14 @@ function Features() {
 
       {/* Section background (needs .relative class on parent and next sibling elements) */}
       <div className="absolute inset-0 bg-gray-100 pointer-events-none mb-16" aria-hidden="true"></div>
-      <div className="absolute left-0 right-0 m-auto w-px p-px h-20 bg-gray-200 transform -translate-y-1/2"></div>
-
+      
       <div className="relative w-full  mx-auto px-4 sm:px-16">
         <div className="pt-12 md:pt-20">
 
           {/* Section header */}
           <div className="w-full mx-auto text-center pb-12 md:pb-16">
             <h1 className="h2 mb-4">What Make us Unique</h1>
-            <p className="text-xl text-gray-600">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur excepteur sint occaecat cupidatat.</p>
+            <p className="text-xl text-gray-600 font-sans">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur excepteur sint occaecat cupidatat.</p>
           </div>
 
           {/* Section content */}
--- src/partials/HeroHome.jsx
+++ src/partials/HeroHome.jsx
@@ -45,7 +45,7 @@ import { Autoplay, Pagination, Navigation } from "swiper";
     <section className="relative w-full">
 
       {/* Illustration behind hero content */}
-      <div className="absolute left-1/2 transform -translate-x-1/2 bottom-0 pointer-events-none" aria-hidden="true">
+      <div className="absolute left-1/2 transform -translate-x-1 bottom-0 pointer-events-none" aria-hidden="true">
         <svg width="1360" height="578" viewBox="0 0 1360 578" xmlns="http://www.w3.org/2000/svg">
           <defs>
             <linearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="illustration-01">
@@ -64,7 +64,7 @@ import { Autoplay, Pagination, Navigation } from "swiper";
       <div className="w-full mx-auto px-4 sm:px-6">
 
         {/* Hero content */}
-        <div className="pt-32 pb-12 md:pt-40 md:pb-20">
+        <div className="pt-20 pb-12 md:pt-28 md:pb-20">
 
           {/* Section header */}
           <div className="text-center pb-4 md:pb-8">
